626c87c62086ec90b5129bec33de29303b1f0f85
Quentin Sedmi <quentinsedmi@gmail.com>
2019-05-06 11:52:48 +0200

    Sort improvements

--- src/components/SortCheckbox.js
+++ src/components/SortCheckbox.js
@@ -4,8 +4,8 @@ export default (props) =>
   <div className='sortCheckbox'>
     <form>
       <label for='sortPopulation'>Sort by population</label>
-      <input type='radio' name='sort' id="sortPopulation" value="sortPopulation" onchange={ () => { props.actions.sortByPopulation() }}/>
+      <input type='radio' name='sort' id="sortPopulation" value="sortPopulation" onchange={ () => { props.actions.sortBy('population') }}/>
       <label for='sortGini'>Sort by gini</label>
-      <input type='radio' name='sort' id="sortGini" value="sortGini" onchange={ () => props.actions.sortByGini() }/>
+      <input type='radio' name='sort' id="sortGini" value="sortGini" onchange={ () => props.actions.sortBy('gini') }/>
     </form>
   </div>
