49ac72cdd57bc3fbb53a21e8ddc31814af70ac2f
aubane marquer <aubane.marquer@gmail.com>
2019-05-13 11:51:10 +0200

    number display

--- src/components/Country.js
+++ src/components/Country.js
@@ -16,11 +16,11 @@ function renderCountry (item) {
       </li>
       <li className='detailCountry'>
         <span className='spanTitle'>Area</span>
-        <span>{ item.area }km&#xB2;</span>
+        <span>{ item.area.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,') }km&#xB2;</span>
       </li>
       <li className='detailCountry'>
         <span className='spanTitle'>Population</span>
-        <span>{ item.population }</span>
+        <span>{ truncateNumber(item.population) }</span>
       </li>
       <li className='detailCountry'>
         <span className='spanTitle'>Population Density</span>
@@ -39,6 +39,13 @@ function renderCountry (item) {
         <span>{ item.currencies[0]['name'] }</span>
       </li>
     </ul>)
+}
+
+const truncateNumber = x => {
+  if (x > 999999999) return x.toString().substring(0, 1) + '.' + x.toString().substring(1, 2) + 'B'
+  if (x > 999999) return x.toString().substring(0, 1) + '.' + x.toString().substring(1, 2) + 'M'
+  if (x > 999) return x.toString().substring(0, 1) + '.' + x.toString().substring(1, 2) + 'K'
+  return x.toString()
 }
 
 export default (props) =>
